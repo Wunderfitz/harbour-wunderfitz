aea99bfebc63b36dd795e344291445bfee5f689e
Åke Engelbrektson <eson57@users.noreply.github.com>
2017-01-12 08:51:41 +0100

    Create harbour-wunderfitz-sv.ts

---
+++ translations/harbour-wunderfitz-sv.ts
@@ -0,0 +1,129 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>About Wunderfitz</source>
+        <translation>Om Wunderfitz</translation>
+    </message>
+    <message>
+        <source>By Sebastian J. Wolf</source>
+        <translation>Av Sebastian J. Wolf</translation>
+    </message>
+    <message>
+        <source>Credits</source>
+        <translation>Tack</translation>
+    </message>
+    <message>
+        <source>Licensed under GNU GPLv2</source>
+        <translation>Licensierad under GNU GPLv2</translation>
+    </message>
+    <message>
+        <source>Sources on GitHub</source>
+        <translation>Källkod på GitHub</translation>
+    </message>
+    <message>
+        <source>Send E-Mail</source>
+        <translation>Skicka e-post</translation>
+    </message>
+    <message>
+        <source>Open heinzelnisse.info</source>
+        <translation>Öppna heinzelnisse.info</translation>
+    </message>
+    <message>
+        <source>Open QuaZIP home page</source>
+        <translation>Öppna QuaZIP hemsida</translation>
+    </message>
+    <message>
+        <source>Open dict.cc home page</source>
+        <translation>Öppna dict.cc hemsida</translation>
+    </message>
+    <message>
+        <source>This application supports dict.cc dictionary export files. Thanks to Paul Hemetsberger and the dict.cc team for providing the dictionaries!</source>
+        <translation>Denna app stödjer cc.dictionarys exportfiler. Tack till Paul Hemetsberger och dict.cc team, för tillhandahållande av ordböckerna!</translation>
+    </message>
+    <message>
+        <source>This application uses the QuaZIP library, version 0.7.2, to extract ZIP archives. Thanks to Sergey A. Tachenov and the QuaZIP contributors for making it so simple to extract ZIP files with Qt!</source>
+        <translation>Denna app använder QuaZIP-bibliotek, version 0.7.2, för att extrahera ZIP-arkiv. Tack till Sergey A. Tachenov och QuaZIP-medarbetare för att ni gjorde det enkelt att extrahera ZIP-filer med Qt!</translation>
+    </message>
+    <message>
+        <source>This project uses the Norwegian-German dictionary from heinzelnisse.info - Updated on January 7, 2017 - Thanks to the authors Heiko Klein and Julia Emmerich for making the dictionary available under the conditions of the GNU GPLv2!</source>
+        <translation>Projektet använder den Norsk-Tyska ordboken från heinzelnisse.info - Uppdaterad 7 Januari 2017 - Tack till utvecklarna Heiko Klein och Julia Emmerich för att ni gjorde ordboken tillgänglig under vilkoren för GNU GPLv2!</translation>
+    </message>
+    <message>
+        <source>A mobile dictionary application for offline use including the Heinzelnisse database and supporting dict.cc export files.</source>
+        <translation>En mobil ordboksapp för offline-användning, som inkluderar Heinzelnisse-databasen och stödjer dict.cc exportfiler.</translation>
+    </message>
+</context>
+<context>
+    <name>DictionariesPage</name>
+    <message>
+        <source>Dictionaries</source>
+        <translation>Ordböcker</translation>
+    </message>
+    <message>
+        <source></source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Download dict.cc dictionaries</source>
+        <translation>Ladda ner ordböcker från dict.cc</translation>
+    </message>
+    <message>
+        <source>Import dict.cc ZIP archives</source>
+        <translation>Importera ZIP-arkiv från dict.cc</translation>
+    </message>
+    <message>
+        <source>Dictionary</source>
+        <translation>Ordbok</translation>
+    </message>
+    <message>
+        <source>Dict.cc Import</source>
+        <translation>Dict.cc-import</translation>
+    </message>
+    <message>
+        <source>Choose the active dictionary here</source>
+        <translation>Välj aktiv ordbok här</translation>
+    </message>
+    <message>
+        <source>Dict.cc does not allow other applications such as Wunderfitz to ship their dictionaries. Therefore, you must download them from dict.cc yourself. Use the Download link, follow the instructions and import the files here afterwards. The downloaded dict.cc ZIP files must be placed in the Downloads folder. If in doubt, use the SailfishOS E-Mail and Browser apps to store the downloads there automatically. After the import in Wunderfitz you can delete the ZIP archives. Please note that you only need to download one combination of two languages. For example if you use DE-EN, you don&apos;t need EN-DE as Wunderfitz always searches in both languages.</source>
+        <translation>Dict.cc tillåter inte andra program såsom Wunderfitz, att leverera deras ordböcker. Därför måste du själv ladda ner dem från dict.cc. Använd nerladdningslänken, följ instruktionerna och importera därefter filerna här. De nerladdade ZIP-filerna måste placeras i mappen &quot;Nedladdningar&quot;. Om du är osäker kan du använda SailfishOS e-postprogram och webbläsare för att automatiskt lagra nerladdningarna där. Efter import i Wunderfitz kan du radera ZIP-arkiven. Notera att du bara behöver ladda ner en av två språkkombinationer. Om du t.ex. vill använda SV-EN, behöver du inte EN-SV, eftersom Wunderfitz alltid söker i bägge språken.</translation>
+    </message>
+</context>
+<context>
+    <name>TitlePage</name>
+    <message>
+        <source>About Wunderfitz</source>
+        <translation>Om Wunderfitz</translation>
+    </message>
+    <message>
+        <source>Search in dictionary...</source>
+        <translation>Sök i ordboken...</translation>
+    </message>
+    <message>
+        <source>Dictionaries</source>
+        <translation>Ordböcker</translation>
+    </message>
+    <message>
+        <source>Searching...</source>
+        <translation>Söker...</translation>
+    </message>
+    <message>
+        <source>Copy to clipboard</source>
+        <translation>Kopiera till urklipp</translation>
+    </message>
+    <message>
+        <source>Search for &apos;%1&apos;</source>
+        <translation>Sök efter &quot;%1&quot;</translation>
+    </message>
+    <message>
+        <source>No results found</source>
+        <translation>Inga träffar</translation>
+    </message>
+    <message>
+        <source>Pull down the menu to import and change your dictionaries</source>
+        <translation>Dra ner menyn för att importera och ändra dina ordböcker</translation>
+    </message>
+</context>
+</TS>
